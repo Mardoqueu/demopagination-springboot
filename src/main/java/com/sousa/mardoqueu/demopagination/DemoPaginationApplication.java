89b2233952e314e6b41973b0aef4846534b35ce4
Mardoqueu Sousa <mdq767@gmail.com>
2023-01-10 12:46:08 -0300

    first commit

---
+++ src/main/java/com/sousa/mardoqueu/demopagination/DemoPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.sousa.mardoqueu.demopagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPaginationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPaginationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sousa/mardoqueu/demopagination/DemoPaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sousa.mardoqueu.demopagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPaginationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
